109d3bb450ec25bd26969af4e86c5b3dad47edf5
Abigail Smith <a.smith58@rgu.ac.uk>
2025-01-30 12:03:38 +0000

    app.post lab 11

--- lab_11/server.js
+++ lab_11/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express(); 
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 
 app.get('/', function(req, res){ 
    res.send("Hello world! by express");
@@ -48,5 +49,10 @@ app.get('/getform', function(req, res){
     res.send("Hi "+name+" I am sure you will "+quest) ; 
 });
     
+app.post('/postform', function(req, res){ 
+    var name = req.body.name; 
+    var quest = req.body.quest; 
+    res.send("Hi "+name+" I am sure you will "+quest) ; 
+});
 
 app.listen(8080); 
